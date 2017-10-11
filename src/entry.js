3f97515d7504ea14c36d7b7ea6e184a883cd4234
mars <mars@huoban.com>
2017-10-11 14:34:55 +0800

    setup

---
+++ src/entry.js
@@ -0,0 +1,8 @@
+import "../less/main.less"
+import React from "react";
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  (<div className="myDiv">Hello Electron2!</div>),
+  document.getElementById("content")
+);
